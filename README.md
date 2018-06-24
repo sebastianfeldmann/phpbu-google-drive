0a09eaf9376fac10d2a0e3f85213406ddcd62da8
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2018-06-24 13:51:27 +0200

    Setup phpbu google drive utility

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# phpbu-google-drive
-Tiny CLI script to generate the google drive authentication file
+# PHPBU Google Drive utility
+With this cli script you can create a Google Drive access token
+file that is required by PHPBU to sync your files to Google Drive.
+
+All you need is a Google api client json credential file that you can easily
+generate online in you Google Drive account settings.
+
+## Installation
+
+There are two ways to install the phpbu Google Drive utility.
+
+### Composer
+
+    composer require phpbu/google-drive-util
+    vendor/bin/phpbu-gdu
+    
+### git
+
+    git clone https://github.com/sebastianfeldmann/phpbu-google-drive.git
+    cd phpbu-google-drive
+    ./phpbu-gdu
+    
+## Usage
+
+    phpbu-gdu [client_secret.json] [client_access.json]
+    
+The first argument is the path to you local client_secret.json that you got from
+your Google Drive account settings.
+If not provided it defaults to `./client_secret.json`.
+
+The second argument is the path where the access token should be stored.
+If not provided it defaults to `./client_access.json`.
---
+++ src/Cmd.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * This file is part of phpbu.
+ *
+ * (c) Sebastian Feldmann <sebastian@phpbu.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace phpbu\GDU;
+
+use Google_Client;
+use Google_Service_Drive;
+
+/**
+ * Class Cmd
+ *
+ * @package phpbu\GDU
+ * @author  Sebastian Feldmann <sebastian@phpbu.de>
+ * @link    https://github.com/sebastianfeldmann/phpbu-google-drive
+ * @since   Class available since Release 0.9.0
+ */
+class Cmd
+{
+    const VERSION = '0.9.0';
+
+    /**
+     * Create google access file.
+     *
+     * @param array $args
+     */
+    public function run(array $args)
+    {
+        printf("phpbu-gdu %s by Sebastian Feldmann and contributors.\n\n", self::VERSION);
+        try {
+            $this->help($args);
+            $secretFile  = $this->getSecretPath($args);
+            $accessFile  = $this->getAccessPath($args);
+            $apiClient   = $this->createClient($secretFile);
+            $accessToken = $this->getAccessToken($accessFile, $apiClient);
+
+            $apiClient->setAccessToken($accessToken);
+
+        } catch (\Exception $e) {
+            printf("%s\n", $e->getMessage());
+            exit(1);
+        }
+
+        printf("Find you credentials file at: %s\n", $accessFile);
+        exit(0);
+    }
+
+    /**
+     * Print help message.
+     *
+     * @param array $args
+     */
+    private function help(array $args)
+    {
+        $help = $args[1] ?? '';
+
+        if (in_array($help, ['-h', '--help'])) {
+            echo <<<EOT
+Usage: phpbu-gdu [client_secret.json] [client_access.json]
+
+  -h, --help    Print this usage information
+
+EOT;
+            exit(0);
+        }
+    }
+
+    /**
+     * Check cli arguments for secret json path.
+     *
+     * @param  array $args
+     * @return string
+     */
+    private function getSecretPath(array $args)
+    {
+        $secret = $args[1] ?? 'client_secret.json';
+        if (!file_exists($secret)) {
+            throw new \RuntimeException('could not find authentication file: ' . $secret);
+        }
+
+        return $secret;
+    }
+
+    /**
+     * Check cli arguments for access json path.
+     *
+     * @param  array $args
+     * @return string
+     */
+    private function getAccessPath(array $args)
+    {
+        return $args[2] ?? 'client_access.json';
+    }
+
+    /**
+     * Setup google api client.
+     *
+     * @param  string $secret
+     * @return \Google_Client
+     * @throws \Google_Exception
+     */
+    private function createClient(string $secret) : Google_Client
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('phpbu');
+        $client->setScopes(Google_Service_Drive::DRIVE);
+        $client->setAuthConfig($secret);
+        $client->setAccessType('offline');
+
+        return $client;
+    }
+
+    /**
+     * Create or update the access file.
+     *
+     * @param  string         $accessFile
+     * @param  \Google_Client $apiClient
+     * @return array
+     */
+    private function getAccessToken(string $accessFile, Google_Client $apiClient) : array
+    {
+        // Load previously authorized credentials from a file.
+        if (file_exists($accessFile)) {
+            $accessToken = json_decode(file_get_contents($accessFile), true);
+        } else {
+            // Request authorization from the user.
+            $authUrl = $apiClient->createAuthUrl();
+            printf("Open the following link in your browser:\n\n%s\n\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an accessFile token.
+            $accessToken = $apiClient->fetchAccessTokenWithAuthCode($authCode);
+
+            // Store the credentials to disk.
+            if (!file_exists(dirname($accessFile))) {
+                mkdir(dirname($accessFile), 0700, true);
+            }
+            file_put_contents($accessFile, json_encode($accessToken));
+        }
+        return $accessToken;
+    }
+
+    /**
+     * Main method, is called by the cli command.
+     */
+    public static function main()
+    {
+        $app = new static();
+        $app->run($_SERVER['argv']);
+    }
+}
